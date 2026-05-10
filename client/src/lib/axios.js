93129c41932e2404ba2a2a21ea2c36c70cd3e39b
mayank188 <mayanksom36@gmail.com>
2026-05-11 01:42:51 +0530

    fixed backend api url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://skillbridge-backend-05fn.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
